2d8154ac7260c7cfb9ecee1aa15123eca284f7a3
Xenon <alguirre2@gmail.com>
2025-03-12 23:49:22 -0600

    01 back set up docker for django with postgresql,etc

---
+++ djangobnb_backend/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ] 
+then
+    echo "check if database is runnning..."
+
+    while ! nc -z $SQL_HOST  $SQL_PORT ; do
+        sleep 0.1
+    done 
+
+    echo "the database is up and runnint :D"
+fi
+
+python manage.py migrate
+
+
+exec "$@"
