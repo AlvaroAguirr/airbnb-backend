2a86ca83eb3526a6473558e18230208970e6f0b0
Xenon <alguirre2@gmail.com>
2025-03-26 17:32:05 -0600

    11 book property

--- djangobnb_backend/property/serializers.py
+++ djangobnb_backend/property/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Property
+from .models import Property, Reservation
 
 from useraccount.serializer import UserDetailSerializer
 
@@ -32,4 +32,18 @@ class PropertiesDetailSerializer(serializers.ModelSerializer):
             'guest',
             'landlord',
 
+        )
+
+
+class ReservartionListSerializer(serializers.ModelSerializer):
+    property = PropertiesListSerializer(read_only=True,many=False)
+    class Meta:
+        model= Reservation
+        fields=(
+            'id',
+            'start_date',
+            'end_date',
+            'number_of_nights',
+            'total_price',
+            'property'
         )
