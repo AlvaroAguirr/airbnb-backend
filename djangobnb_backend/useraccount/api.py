ad134cc0c91618f7c447b3a544aa7a99e9353628
Xenon <alguirre2@gmail.com>
2025-03-26 18:20:58 -0600

    12 landlord page dynamic

---
+++ djangobnb_backend/useraccount/api.py
@@ -0,0 +1,18 @@
+from .serializer import UserDetailSerializer
+
+from django.http import JsonResponse
+from .models import User
+
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def landlord_detail(request,pk):
+    user= User.objects.get(pk=pk)
+
+    serializer = UserDetailSerializer(user,many= False)
+
+    return JsonResponse (serializer.data, safe=False)
+
