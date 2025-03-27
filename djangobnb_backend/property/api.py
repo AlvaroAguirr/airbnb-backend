731d18be5ea3b3264df0e862b875271dddd598ca
Xenon <alguirre2@gmail.com>
2025-03-26 20:51:13 -0600

    14 make it posible to set property as favorite

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -1,30 +1,60 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-
+from rest_framework_simplejwt.tokens import AccessToken
 
 from . forms import PropertyForm
 from .models import Property, Reservation
 from .serializers import PropertiesListSerializer, PropertiesDetailSerializer, ReservartionListSerializer
-
+from useraccount.models import User
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
 def properties_list(request):
-    properties = Property.objects.all()
+
+   #
+   # auth
+   try:
+      token= request.META ['HTTP_AUTHORIZATION'].split('Bearer ')[1]
+      token=AccessToken(token)
+      user_id= token.payload['user_id']
+      user= User.objects.get(pk=user_id)
+   except Exception as e:
+      user= None
+      
+   print('user', user)
+
+   #
+   #
+
+
+   favorites= []
+   properties = Property.objects.all()
 
     #
     # FIlter
 
-    landlord_id=request.GET.get('landlord_id', '')
-    if landlord_id:
-       properties=properties.filter(landlord_id=landlord_id)
+   landlord_id=request.GET.get('landlord_id', '')
+   if landlord_id:
+      properties=properties.filter(landlord_id=landlord_id)
+   #
+   # favorites
+   if user:
+      for property in properties:
+         if user in property.favorited.all():
+            favorites.append(property.id)
+
+   print('favorite',favorites)
+            
+
+   #
+   #
 
 
+   serializer= PropertiesListSerializer(properties, many= True)
 
-    serializer= PropertiesListSerializer(properties, many= True)
-
-    return JsonResponse ( {
-        'data': serializer.data
+   return JsonResponse ( {
+        'data': serializer.data, 
+        'favorites': favorites
     })
 
 @api_view(['GET'])
@@ -91,3 +121,18 @@ def book_property(request,pk):
       print('Error', e)
 
       return JsonResponse({'success del back':False})
+
+
+@api_view(['POST'])
+def toggle_favorite(request, pk):
+   property = Property.objects.get(pk=pk)
+
+   if request.user in property.favorited.all():
+      property.favorited.remove(request.user)
+
+      return JsonResponse ({'is_favorite': False })
+   else:
+      property.favorited.add(request.user)
+
+      return JsonResponse ({'is_favorite': True })
+   
