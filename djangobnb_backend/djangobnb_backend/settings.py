bb6ab197fd07f0f13433800ac3760370b2667293
Xenon <alguirre2@gmail.com>
2025-03-27 17:37:49 -0600

    17 get up sockets in the backend

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -25,6 +25,12 @@ AUTH_USER_MODEL='useraccount.User'
 
 SITE_ID= 1 
 WEBSITE_URL='http://localhost:8000'
+
+CHANNEL_LAYERS={
+    'default':{
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
 SIMPLE_JWT={
     "ACCESS_TOKEN_LIFETIME":timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME":timedelta(days=7),
@@ -68,6 +74,7 @@ REST_AUTH = {
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,7 +128,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'djangobnb_backend.wsgi.application'
-
+ASGI_APPLICATION = 'djangobnb_backend.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
